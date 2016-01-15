97108384bf69d19282ab22e74c2c895e923d6a7e
Jake Lin <jakelinau@gmail.com>
2016-01-15 22:44:30 +1100

    Add Carthage project.

---
+++ Carthage/IBAnimatable-Carthage/IBAnimatable-Carthage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBAnimatable-Carthage
+//
+//  Created by Jake Lin on 1/15/16.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # IBAnimatable-PackageManagementExamples
-example projects to demonstrate how to use IBAnimatable with different Package Management tools.
+Example projects to demonstrate how to use IBAnimatable with different Package Management tools.
