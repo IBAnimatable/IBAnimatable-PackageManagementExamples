4ee1c13c83ae646e31abb54533287a67ed8f950c
Jake Lin <JakeLinAu@gmail.com>
2016-03-28 16:00:53 +1100

    Add Carthage 0.15.2 support

--- IBAnimatable-Carthage/IBAnimatable-Carthage/ViewController.swift
+++ IBAnimatable-Carthage/IBAnimatable-Carthage/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    let aView = AnimatableView(frame: .zero)
+    view.addSubview(aView)
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
 
